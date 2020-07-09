0e42bb2bb5a333d9870e8a36ef0a75b924e85478
Kamil Bechta <kamil.bechta1@gmail.com>
2020-07-09 21:55:44 +0200

    Add ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const ErrorDiv = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+
+  p {
+    font-size: 12rem;
+  }
+  span {
+    font-size: 6rem;
+  }
+`;
 
 export interface ErrorPageProps {}
 
 const ErrorPage: React.SFC<ErrorPageProps> = () => {
-  return <div>404</div>;
+  return (
+    <ErrorDiv>
+      <p>404</p>
+      <span>Page Not Found</span>
+    </ErrorDiv>
+  );
 };
 
 export default ErrorPage;
