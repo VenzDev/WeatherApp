0aa169163651f9fefd9f9be5007e99dc62c81263
Kamil Bechta <kamil.bechta1@gmail.com>
2020-07-11 16:15:22 +0200

    Add show cities from user data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,25 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import HomeContainer from "../components/HomeContainer";
 import { cities } from "../redux/cities";
 import showToast from "../utils/showToast";
 
 const HomePage: React.SFC = () => {
   const dispatch = useDispatch();
-  const ids: Array<String> = ["2643743", "759734"];
+  const userData = useSelector((state: any) => state.userReducer);
+  const { favCities } = userData.user;
   useEffect(() => {
+    //FIXED bug with login again!
+    dispatch(cities.clearData());
     showToast("Login Success");
-    ids.forEach((id) => dispatch(cities.getCityMethod(id)));
+    favCities.forEach((id: String) => dispatch(cities.getCityMethod(id)));
     const interval = setInterval(() => {
       dispatch(cities.clearData());
-      ids.forEach((id) => dispatch(cities.getCityMethod(id)));
+      favCities.forEach((id: String) => dispatch(cities.getCityMethod(id)));
     }, 1000 * 10);
 
     return () => clearInterval(interval);
-  }, [dispatch, ids]);
+  }, [dispatch, favCities]);
 
   return <HomeContainer></HomeContainer>;
 };
