35635f2241bb9bccd656950751c9ca84dc9dcba3
Kamil Bechta <kamil.bechta1@gmail.com>
2020-07-12 16:37:03 +0200

    Add delete city from favourite list

--- src/components/CityCard/CityCard.tsx
+++ src/components/CityCard/CityCard.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import cityImage from "../../images/city.png";
 import styled from "styled-components";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cities } from "../../redux/cities";
+import { user } from "../../redux/user";
 
 export interface CityCardProps {
   city: {
@@ -18,17 +19,33 @@ const Div = styled.div`
     cursor: pointer;
   }
 `;
+const Button = styled.button`
+  position: absolute;
+  right: 0;
+  top: 0;
+`;
 
 const CityCard: React.SFC<CityCardProps> = ({ city }) => {
+  const userData = useSelector((state: any) => state.userReducer);
   const dispatch = useDispatch();
   const handleSelectCity = () => dispatch(cities.selectCity(city));
+  const handleDelete = () => {
+    const newFavCities = userData.user.favCities.filter(
+      (userCityId: string) => city.id.toString() !== userCityId
+    );
+    dispatch(user.updateCities(userData.user.id, newFavCities));
+  };
 
   return (
-    <Div onClick={handleSelectCity} className="col-xl-2 col-lg-4 col-md-6 mb-3">
+    <Div className="col-xl-2 col-lg-4 col-md-6 mb-3">
       <div style={{ borderRadius: "10px" }} className="card shadow">
         <div className="card-body  p-0">
           <div>
+            <Button onClick={handleDelete} className="btn btn-danger">
+              x
+            </Button>
             <img
+              onClick={handleSelectCity}
               style={{ width: "100%", height: "170px" }}
               src={cityImage}
               alt="city"
